0e1396c98bc2eb4d0a2c20b0aaf01adb05523154
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-11-29 15:27:56 +0530

    [Nithin] Add : Updation of code is done

---
+++ FundooApplication.Api/FundooApplication/Controllers/UserController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FundooManager.IManager;
+using FundooModel.User;
+using System.Threading.Tasks;
+using System;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
+namespace FundooApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly IUserManager userManager;
+        private readonly IConfiguration configuration;
+
+        public UserController(IUserManager userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<ActionResult> UserRegister(Register register)
+        {
+            try
+            {
+                var result = await this.userManager.RegisterUser(register);
+                if (result == 1)
+                {
+                    return this.Ok(new { Status = true, Message = "User Registration Successful", data = register });
+                }
+                return this.BadRequest(new { Status = false, Message = "User Registration UnSuccessful" });
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult UserLogin(Login login)
+        {
+            try
+            {
+                var result = this.userManager.LoginUser(login);
+                if (result != null)
+                {
+                    var tokenhandler = new JwtSecurityTokenHandler();
+                    var jwtToken = tokenhandler.ReadJwtToken(result);
+                    var id = jwtToken.Claims.FirstOrDefault(c => c.Type == "Id");
+                    string Id = id.Value;
+
+
+                    return this.Ok(new { Status = true, Message = "User Login Successful", Data = result ,id= Id });
+                }
+                return this.BadRequest(new { Status = false, Message = "User Login UnSuccessful" });
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+        [Authorize]
+        [HttpPut]
+        [Route("ResetPassword")]
+        public ActionResult UserResetPassword(string newPassword ,string confirmPassword)
+        {
+            try
+            {
+                var email = User.FindFirst(ClaimTypes.Email).Value.ToString();
+                var result = this.userManager.ResetPassword(email,newPassword,confirmPassword);
+                if (result != null)
+                {
+                    return this.Ok(new { Status = true, Message = "User password reset Successful", data = result });
+                }
+                return this.BadRequest(new { Status = false, Message = "User password reset UnSuccessful" });
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = false, Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("ForgetPassword")]
+        public ActionResult ForgetPassword(string email)
+        {
+            try
+            {
+                var resultLog = this.userManager.ForgetPassword(email);
+                if (resultLog != null)
+                {
+                    return Ok(new { success = true, message = "Reset Email Send" });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Reset UnSuccessful" });
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        private string GenerateJWTToken(int userId, string email)
+        {
+            var secretkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Key"]));
+            var signinCredentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+      {
+          new Claim(ClaimTypes.Role, "user"),
+          new Claim("Id", userId.ToString()),
+          new Claim("Email", email)
+      };
+            var tokenOptionOne = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: signinCredentials);
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenOptionOne);
+            return token;
+        }
+    }
+}
